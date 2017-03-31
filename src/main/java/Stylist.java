1b0573f0a937ac445c8bafb23ee8a7276bf95f22
Chance <chancemagno@gmail.com>
2017-03-31 09:44:20 -0700

    add test and methods to check if members of Stylist are instantiated

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,9 +1,39 @@
 import org.sql2o.*;
 
 public class Stylist {
+  int id;
+  String firstName;
+  String lastName;
+  String phoneNumber;
+  String email;
+  String hireDate;
 
-  public Stylist() {
+  public Stylist(String firstName, String lastName, String phoneNumber, String email, String hireDate) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+    this.hireDate = hireDate;
+  }
 
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getHireDate() {
+    return hireDate;
   }
 
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -9,7 +9,19 @@ public class StylistTest {
 
   @Test
   public void stylistClass_instantiatesCorrectly_true() {
-    Stylist test = new Stylist();
+    Stylist test = new Stylist("firstname", "lastname", "phonenumber", "email", "hiredate");
     assertTrue(test instanceof Stylist);
+  }
+
+  @Test
+  public void stylistInstantiateswithMembers_true() {
+    Stylist test = new Stylist("firstname", "lastname", "phonenumber", "email", "hiredate");
+    assertEquals("firstname", test.getFirstName());
+    assertEquals("lastname", test.getLastName());
+    assertEquals("phonenumber", test.getPhoneNumber());
+    assertEquals("email", test.getEmail());
+    assertEquals("hiredate", test.getHireDate());
+
+
   }
 }
