df841caf90733fbf4788c1de69127d74f136905f
chance <chancemagno@gmail.com>
2017-03-31 13:31:17 -0700

    create basic stylist test and methods all passing

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,39 +1,80 @@
 import org.sql2o.*;
+import java.util.List;
 
 public class Stylist {
   int id;
-  String firstName;
-  String lastName;
-  String phoneNumber;
+  String first_name;
+  String last_name;
+  String phone_number;
   String email;
-  String hireDate;
 
-  public Stylist(String firstName, String lastName, String phoneNumber, String email, String hireDate) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.phoneNumber = phoneNumber;
+  public Stylist(String first_name, String last_name, String phone_number, String email) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.phone_number = phone_number;
     this.email = email;
-    this.hireDate = hireDate;
   }
 
   public String getFirstName() {
-    return firstName;
+    return first_name;
   }
 
   public String getLastName() {
-    return lastName;
+    return last_name;
   }
 
   public String getPhoneNumber() {
-    return phoneNumber;
+    return phone_number;
   }
 
   public String getEmail() {
     return email;
   }
 
-  public String getHireDate() {
-    return hireDate;
+  public int getId() {
+    return id;
   }
+
+  public static List<Stylist> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM stylists";
+      return con.createQuery(sql).executeAndFetch(Stylist.class);
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO stylists (first_name, last_name, phone_number, email) VALUES (:first_name, :last_name, :phone_number, :email);";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("first_name", this.first_name)
+      .addParameter("last_name", this.last_name)
+      .addParameter("phone_number", phone_number)
+      .addParameter("email", email)
+      .executeUpdate()
+      .getKey();
+
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherStylist){
+    if(!(otherStylist instanceof Stylist)) {
+      return false;
+    } else {
+      Stylist newStylist = (Stylist) otherStylist;
+      return this.getFirstName().equals(newStylist.getFirstName()) && this.getLastName().equals(newStylist.getLastName()) && this.getPhoneNumber().equals(newStylist.getPhoneNumber()) &&
+      this.getEmail().equals(newStylist.getEmail());
+    }
+  }
+
+  public static Stylist find(int id) {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "SELECT * FROM stylists WHERE id = :id;";
+    Stylist stylist = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Stylist.class);
+    return stylist;
+  }
+}
 
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -9,19 +9,40 @@ public class StylistTest {
 
   @Test
   public void stylistClass_instantiatesCorrectly_true() {
-    Stylist test = new Stylist("firstname", "lastname", "phonenumber", "email", "hiredate");
+    Stylist test = new Stylist("firstname", "lastname", "phonenumber", "email");
     assertTrue(test instanceof Stylist);
   }
 
   @Test
   public void stylistInstantiateswithMembers_true() {
-    Stylist test = new Stylist("firstname", "lastname", "phonenumber", "email", "hiredate");
+    Stylist test = new Stylist("firstname", "lastname", "phonenumber", "email");
     assertEquals("firstname", test.getFirstName());
     assertEquals("lastname", test.getLastName());
     assertEquals("phonenumber", test.getPhoneNumber());
     assertEquals("email", test.getEmail());
-    assertEquals("hiredate", test.getHireDate());
+  }
+  @Test
+  public void save_returnsTrueIfStylistsAreTheSame() {
+    Stylist newStylist = new Stylist("firstname", "lastname", "phonenumber", "email");
+    newStylist.save();
+    assertTrue(Stylist.all().get(0).equals(newStylist));
+  }
 
-
+  @Test
+  public void all_returnsAllStylist_true() {
+    Stylist test = new Stylist("firstname", "lastname", "phonenumber", "email");
+    Stylist test1 = new Stylist("Jim", "Strong", "3609107874", "JimStronggmailcom");
+    test.save();
+    test1.save();
+    assertTrue(test.equals(Stylist.all().get(0)));
+    assertTrue(test1.equals(Stylist.all().get(1)));
+  }
+  @Test
+  public void find_returnsStylistWithSameId_test2() {
+    Stylist stylist1 = new Stylist("firstname", "lastname", "phonenumber", "email");
+    stylist1.save();
+    Stylist stylist2 = new Stylist("Jim", "Strong", "3609107874", "JimStronggmailcom");
+    stylist2.save();
+    assertEquals(Stylist.find(stylist2.getId()), stylist2);
   }
 }
